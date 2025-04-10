1caa81fbc642c951d18587d768052ef7f2a3351d
Rei <137615290+Reiventsu@users.noreply.github.com>
2025-04-10 18:52:31 +0200

    managed to successfully use raylib to open an example window!

--- Projects/Games/SnakeGame/main.cpp
+++ Projects/Games/SnakeGame/main.cpp
@@ -2,3 +2,19 @@
 // Created by rei on 4/9/25.
 //
 #include "pch.h"
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
